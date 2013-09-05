9fc07cbbe036890b0407eca4d106fe4622c1617b
Jorge Ferrer <jorge.ferrer@liferay.com>
2013-09-05 15:17:02 +0200

    LPS-39662 Read cookies that start with a given prefix and make them available anywhere that the User object is available
    This is necessary for example to be able to segment anonymous users. Plugins will be responsible for setting the cookie, but the portal must be able to read this type of cookies (which I'm calling "Remote Preferences") and make them available as part of the User object so that they are easily accessible from from the code.

--- portal-impl/src/com/liferay/portal/util/PortalImpl.java
+++ portal-impl/src/com/liferay/portal/util/PortalImpl.java
@@ -250,6 +250,7 @@ import javax.portlet.WindowState;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletRequestWrapper;
 import javax.servlet.http.HttpServletResponse;
@@ -5218,6 +5219,17 @@ public class PortalImpl implements Portal {
 			user = UserLocalServiceUtil.getUserById(userId);
 
 			request.setAttribute(WebKeys.USER, user);
+		}
+
+		for (Cookie cookie : request.getCookies()) {
+			String cookieName = cookie.getName();
+
+			if (cookieName.startsWith(CookieKeys.REMOTE_PREFERENCES_PREFIX)) {
+				String name = cookieName.substring(
+					CookieKeys.REMOTE_PREFERENCES_PREFIX.length());
+
+				user.addRemotePreference(name, cookie.getValue());
+			}
 		}
 
 		return user;
