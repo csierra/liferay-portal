08941170f4d666aab1d512eb92d92afe1d862530
Miguel Angel Pastor Olivar <miguel.pastor@liferay.com>
2014-02-24 09:10:53 +0100

    Verify resources could be extended by modules

--- portal-impl/src/com/liferay/portal/verify/VerifyResourcePermissions.java
+++ portal-impl/src/com/liferay/portal/verify/VerifyResourcePermissions.java
@@ -49,13 +49,13 @@ import com.liferay.portlet.journal.model.JournalArticle;
 import com.liferay.portlet.journal.model.JournalFeed;
 import com.liferay.portlet.messageboards.model.MBCategory;
 import com.liferay.portlet.messageboards.model.MBMessage;
-import com.liferay.portlet.polls.model.PollsQuestion;
 import com.liferay.portlet.shopping.model.ShoppingCategory;
 import com.liferay.portlet.shopping.model.ShoppingItem;
 import com.liferay.portlet.softwarecatalog.model.SCFrameworkVersion;
 import com.liferay.portlet.softwarecatalog.model.SCProductEntry;
 import com.liferay.portlet.wiki.model.WikiNode;
 import com.liferay.portlet.wiki.model.WikiPage;
+import com.liferay.registry.ServiceTrackerCollection;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -79,6 +79,12 @@ public class VerifyResourcePermissions extends VerifyProcess {
 
 			for (String[] model : _MODELS) {
 				verifyModel(role, model[0], model[1], model[2]);
+			}
+
+			for (ModelResource modelResource : _modelResources) {
+				verifyModel(
+					role, modelResource.getName(), modelResource.getModelName(),
+					modelResource.getPkColumnName());
 			}
 
 			verifyLayout(role);
@@ -268,9 +274,6 @@ public class VerifyResourcePermissions extends VerifyProcess {
 			PasswordPolicy.class.getName(), "PasswordPolicy", "passwordPolicyId"
 		},
 		new String[] {
-			PollsQuestion.class.getName(), "PollsQuestion", "questionId"
-		},
-		new String[] {
 			SCFrameworkVersion.class.getName(), "SCFrameworkVersion",
 			"frameworkVersionId"
 		},
@@ -299,5 +302,7 @@ public class VerifyResourcePermissions extends VerifyProcess {
 
 	private static Log _log = LogFactoryUtil.getLog(
 		VerifyResourcePermissions.class);
+
+	private ServiceTrackerCollection<ModelResource> _modelResources;
 
 }
---
+++ portal-service/src/com/liferay/portal/verify/ModelResource.java
@@ -0,0 +1,11 @@
+package com.liferay.portal.verify;
+
+/**
+ * @author Miguel Pastor
+ */
+public interface ModelResource {
+	public String getName();
+	public String getModelName();
+	public String getPkColumnName();
+
+}
