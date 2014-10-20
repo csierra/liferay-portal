efaa2c43d6998aaf3a57bc86b80f9a8ef57313ca
Carlos Sierra <carlos.sierra@liferay.com>
2014-10-20 15:00:55 +0200

    LPS-47664 Added example TagKeyResolver
    This resolver acts on com.liferay.taglib.aui.FormTag. The resolvers
    assigns key for the tags using the portlet where they are being used
    plus the name attribute of the tag.

---
+++ modules/portal/portal-formtag-extension-enabler/src/com/liferay/view/extensions/resolver/FormTagKeyResolver.java
@@ -0,0 +1,51 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.view.extensions.resolver;
+
+import com.liferay.portal.kernel.bean.BeanPropertiesUtil;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.util.PortalUtil;
+import com.liferay.portal.util.TagKeyResolver;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Carlos Sierra Andrés
+ */
+@Component(
+	immediate = true, property = {"target=com.liferay.taglib.aui.FormTag"},
+	service = TagKeyResolver.class
+)
+public class FormTagKeyResolver implements TagKeyResolver {
+
+	@Override
+	public String getKey(
+		HttpServletRequest request, HttpServletResponse response,
+		Object jspTag) {
+
+		String portletId = PortalUtil.getPortletId(request);
+		String name = BeanPropertiesUtil.getStringSilent(jspTag, "name");
+
+		if (Validator.isNull(portletId) || Validator.isNull(name)) {
+			return null;
+		}
+
+		return portletId.concat("-").concat(name);
+	}
+
+}
