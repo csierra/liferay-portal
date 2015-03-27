7ba36d0a3c15057aa911144e01d8bbb318a9b577
adolfo.perez <adolfo.perez@liferay.com>
2015-03-27 14:56:19 +0100

    LPS-54589 Copy implementation of getMessage from LanguageResources
    The only change wrt the original is that all fields (except constants)
    are no longer static.

--- portal-impl/src/com/liferay/portal/language/LanguageResourceManagerImpl.java
+++ portal-impl/src/com/liferay/portal/language/LanguageResourceManagerImpl.java
@@ -15,11 +15,28 @@
 package com.liferay.portal.language;
 
 import com.liferay.portal.kernel.language.LanguageResourceManager;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.util.CharPool;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.PropertiesUtil;
+import com.liferay.portal.kernel.util.StringBundler;
+import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.util.StringUtil;
 
+import java.io.InputStream;
+
+import java.net.URL;
+
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
+import java.util.Properties;
 import java.util.ResourceBundle;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * @author Adolfo Pérez
@@ -28,7 +45,24 @@ public class LanguageResourceManagerImpl implements LanguageResourceManager {
 
 	@Override
 	public String getMessage(Locale locale, String key) {
-		throw new UnsupportedOperationException();
+		if (locale == null) {
+			return null;
+		}
+
+		Map<String, String> languageMap = _languageMaps.get(locale);
+
+		if (languageMap == null) {
+			languageMap = _loadLocale(locale);
+		}
+
+		String value = languageMap.get(key);
+
+		if (value == null) {
+			return getMessage(getSuperLocale(locale), key);
+		}
+		else {
+			return value;
+		}
 	}
 
 	@Override
@@ -41,6 +75,144 @@ public class LanguageResourceManagerImpl implements LanguageResourceManager {
 			config.replace(CharPool.PERIOD, CharPool.SLASH));
 	}
 
+	protected Locale getSuperLocale(Locale locale) {
+		Locale superLocale = _superLocales.get(locale);
+
+		if (superLocale != null) {
+			if (superLocale == _nullLocale) {
+				return null;
+			}
+
+			return superLocale;
+		}
+
+		superLocale = _getSuperLocale(locale);
+
+		if (superLocale == null) {
+			_superLocales.put(locale, _nullLocale);
+		}
+		else {
+			_superLocales.put(locale, superLocale);
+		}
+
+		return superLocale;
+	}
+
+	private Locale _getSuperLocale(Locale locale) {
+		String variant = locale.getVariant();
+
+		if (variant.length() > 0) {
+			return new Locale(locale.getLanguage(), locale.getCountry());
+		}
+
+		String country = locale.getCountry();
+
+		if (country.length() > 0) {
+			Locale priorityLocale = LanguageUtil.getLocale(
+				locale.getLanguage());
+
+			if ((priorityLocale != null) && !locale.equals(priorityLocale)) {
+				return new Locale(
+					priorityLocale.getLanguage(), priorityLocale.getCountry());
+			}
+
+			return LocaleUtil.fromLanguageId(locale.getLanguage(), false, true);
+		}
+
+		String language = locale.getLanguage();
+
+		if (language.length() > 0) {
+			return _blankLocale;
+		}
+
+		return null;
+	}
+
+	private Map<String, String> _loadLocale(Locale locale) {
+		Map<String, String> languageMap = null;
+
+		if (_configNames.length > 0) {
+			String localeName = locale.toString();
+
+			languageMap = new HashMap<>();
+
+			for (String name : _configNames) {
+				StringBundler sb = new StringBundler(4);
+
+				sb.append(name);
+
+				if (localeName.length() > 0) {
+					sb.append(StringPool.UNDERLINE);
+					sb.append(localeName);
+				}
+
+				sb.append(".properties");
+
+				Properties properties = _loadProperties(sb.toString());
+
+				LanguageResources.fixValues(languageMap, properties);
+			}
+		}
+		else {
+			languageMap = Collections.emptyMap();
+		}
+
+		_languageMaps.put(locale, languageMap);
+
+		return languageMap;
+	}
+
+	private Properties _loadProperties(String name) {
+		Properties properties = new Properties();
+
+		try {
+			ClassLoader classLoader = LanguageResources.class.getClassLoader();
+
+			Enumeration<URL> enu = classLoader.getResources(name);
+
+			if (_log.isDebugEnabled() && !enu.hasMoreElements()) {
+				_log.debug("No resources found for " + name);
+			}
+
+			while (enu.hasMoreElements()) {
+				URL url = enu.nextElement();
+
+				if (_log.isInfoEnabled()) {
+					_log.info("Loading " + name + " from " + url);
+				}
+
+				try (InputStream inputStream = url.openStream()) {
+					Properties inputStreamProperties = PropertiesUtil.load(
+						inputStream, StringPool.UTF8);
+
+					properties.putAll(inputStreamProperties);
+
+					if (_log.isInfoEnabled()) {
+						_log.info(
+							"Loading " + url + " with " +
+								inputStreamProperties.size() + " values");
+					}
+				}
+			}
+		}
+		catch (Exception e) {
+			if (_log.isWarnEnabled()) {
+				_log.warn(e, e);
+			}
+		}
+
+		return properties;
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		LanguageResourceManagerImpl.class);
+
+	private static final Locale _blankLocale = new Locale(StringPool.BLANK);
+	private static final Locale _nullLocale = new Locale(StringPool.BLANK);
+
 	private String[] _configNames;
+	private final Map<Locale, Map<String, String>> _languageMaps =
+		new ConcurrentHashMap<>(64);
+	private final Map<Locale, Locale> _superLocales = new ConcurrentHashMap<>();
 
 }
