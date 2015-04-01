54fc21b89225b7290219979d151e53969fa89f4c
Miguel Angel Pastor Olivar <miguel.pastor@liferay.com>
2015-04-01 12:12:03 +0200

    LPS-54647 Release whiteboard properties

---
+++ portal-service/src/com/liferay/portal/kernel/upgrade/whiteboard/UpgradeWhiteboardConstants.java
@@ -0,0 +1,31 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.kernel.upgrade.whiteboard;
+
+/**
+ * @author Miguel Pastor
+ */
+public final class UpgradeWhiteboardConstants {
+
+	public static final String UPGRADE_WHITEBOARD_RELEASE_NAME =
+		"upgrade.whiteboard.release.name";
+
+	public static final String UPGRADE_WHITEBOARD_RELEASE_PREVIOUS_VERSION =
+		"upgrade.whiteboard.release.previous.version";
+
+	public static final String UPGRADE_WHITEBOARD_RELEASE_VERSION =
+		"upgrade.whiteboard.release.version";
+
+}
