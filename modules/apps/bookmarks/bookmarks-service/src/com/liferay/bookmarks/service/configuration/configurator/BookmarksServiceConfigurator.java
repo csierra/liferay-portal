1d08187987077f902c4fb6776c9f3a6fc36400fb
Carlos Sierra <carlos.sierra@liferay.com>
2015-06-26 13:04:09 +0200

    LPS-54647 Disable DB creation through initServices

--- modules/apps/bookmarks/bookmarks-service/src/com/liferay/bookmarks/service/configuration/configurator/BookmarksServiceConfigurator.java
+++ modules/apps/bookmarks/bookmarks-service/src/com/liferay/bookmarks/service/configuration/configurator/BookmarksServiceConfigurator.java
@@ -27,6 +27,9 @@ import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Deactivate;
 import org.osgi.service.component.annotations.Reference;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
 /**
  * @author Miguel Pastor
  */
@@ -53,7 +56,17 @@ public class BookmarksServiceConfigurator {
 	protected ServiceComponentConfiguration getConfiguration() {
 		Bundle bundle = FrameworkUtil.getBundle(getClass());
 
-		return new ModuleResourceLoader(bundle);
+		return new ModuleResourceLoader(bundle) {
+			@Override
+			public InputStream getSQLIndexesInputStream() {
+				return new ByteArrayInputStream(new byte[0]);
+			}
+
+			@Override
+			public InputStream getSQLTablesInputStream() {
+				return new ByteArrayInputStream(new byte[0]);
+			}
+		};
 	}
 
 	@Reference(
