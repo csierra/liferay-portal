b0f411f7655172959df5018d983b5cf887249de9
Miguel Angel Pastor Olivar <miguel.pastor@liferay.com>
2015-08-04 07:45:16 +0200

    Bookmarks Upgrade

--- modules/apps/bookmarks/bookmarks-service/src/com/liferay/bookmarks/upgrade/BookmarksServiceUpgrade.java
+++ modules/apps/bookmarks/bookmarks-service/src/com/liferay/bookmarks/upgrade/BookmarksServiceUpgrade.java
@@ -33,8 +33,8 @@ import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
 /**
- * @author Carlos Sierra Andrés
  * @author Miguel Pastor
+ * @author Carlos Sierra Andrés
  */
 @Component(
 	immediate = true,
