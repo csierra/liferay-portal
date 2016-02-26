f6ca4687c57b6933a66ab3007920b30eec389c51
Raymond Auge <raymond.auge@liferay.com>
2016-02-26 09:44:57 -0500

    LPS-63730 SF

--- modules/apps/foundation/portal/portal-upgrade/src/main/java/com/liferay/portal/upgrade/internal/release/ReleaseManager.java
+++ modules/apps/foundation/portal/portal-upgrade/src/main/java/com/liferay/portal/upgrade/internal/release/ReleaseManager.java
@@ -278,7 +278,8 @@ public class ReleaseManager {
 
 	private OutputStreamContainerFactoryTracker
 		_outputStreamContainerFactoryTracker;
-	private BlockingDeque<Callable<Void>> _queue = new LinkedBlockingDeque<>();
+	private final BlockingDeque<Callable<Void>> _queue =
+		new LinkedBlockingDeque<>();
 	private ReleaseLocalService _releaseLocalService;
 	private ReleaseManagerConfiguration _releaseManagerConfiguration;
 	private ReleasePublisher _releasePublisher;
@@ -513,7 +514,7 @@ public class ReleaseManager {
 
 		private UpgradeThread() {
 			super("Upgrade Thread");
-			
+
 			start();
 		}
 
