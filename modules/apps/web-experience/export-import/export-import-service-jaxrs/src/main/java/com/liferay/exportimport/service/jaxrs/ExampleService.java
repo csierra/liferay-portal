d6bdd60105e7132e8900a33787719ec95b397ddb
Carlos Sierra <carlos.sierra@liferay.com>
2016-05-31 15:27:26 +0200

    Need Application

--- modules/apps/web-experience/export-import/export-import-service-jaxrs/src/main/java/com/liferay/exportimport/service/jaxrs/ExampleService.java
+++ modules/apps/web-experience/export-import/export-import-service-jaxrs/src/main/java/com/liferay/exportimport/service/jaxrs/ExampleService.java
@@ -1,21 +1,30 @@
 package com.liferay.exportimport.service.jaxrs;
 
-import org.osgi.service.component.annotations.Component;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.Application;
 import javax.ws.rs.core.Response;
 
+import org.osgi.service.component.annotations.Component;
+
+import java.util.Collections;
+import java.util.Set;
+
 @Component(
-	immediate = true, property = {"staging.jax.rs.service=true"},
-	service = ExampleService.class
+	immediate = true, property = {"staging.jaxrs.application=true"},
+	service=Application.class
 )
 @Path("/ExampleService")
-public class ExampleService {
+public class ExampleService extends Application {
+
+	public Set<Object> getSingletons() {
+		return Collections.<Object>singleton(this);
+	}
 
 	@GET
 	public Response myCall() {
-
 		return Response.status(200).entity("myCall is called").build();
 	}
+
 }
