fc02a56a9ae3757ef6b9f0378fb3046f9b2cfbe6
Carlos Sierra <carlos.sierra@liferay.com>
2016-10-04 15:41:01 +0200

    Replicate authetication using secret
    This is a PoC and that's the reason I am duplicating this. We should
    factor this out and reuse or use other system.

--- modules/apps/web-experience/staging/staging-remote-api/src/main/java/com/liferay/staging/remote/api/StagingRemoteClient.java
+++ modules/apps/web-experience/staging/staging-remote-api/src/main/java/com/liferay/staging/remote/api/StagingRemoteClient.java
@@ -15,12 +15,22 @@
 package com.liferay.staging.remote.api;
 
 import com.liferay.exportimport.kernel.model.ExportImportConfiguration;
+import com.liferay.portal.kernel.security.auth.AuthException;
 import com.liferay.portal.kernel.security.auth.HttpPrincipal;
 import com.liferay.portal.kernel.util.Base64;
+import com.liferay.portal.kernel.util.PropsKeys;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.util.PropsValues;
+import com.liferay.util.Encryptor;
+import com.liferay.util.EncryptorException;
+import org.apache.commons.codec.DecoderException;
+import org.apache.commons.codec.binary.Hex;
 
 import java.util.List;
 import java.util.function.Function;
 
+import javax.crypto.spec.SecretKeySpec;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.Invocation;
@@ -35,7 +45,8 @@ import javax.ws.rs.core.MediaType;
 public class StagingRemoteClient {
 
 	public static List<MissingLayoutRepr> checkLayouts(
-		HttpPrincipal httpPrincipal, List<MissingLayoutRepr> parentLayouts) {
+		HttpPrincipal httpPrincipal, List<MissingLayoutRepr> parentLayouts)
+		throws AuthException, EncryptorException {
 
 		return createAuthorizedRequest(
 			httpPrincipal,
@@ -46,7 +57,8 @@ public class StagingRemoteClient {
 	}
 
 	public static void cleanUpStagingRequest(
-		HttpPrincipal httpPrincipal, long stagingRequestId) {
+		HttpPrincipal httpPrincipal, long stagingRequestId)
+		throws AuthException, EncryptorException {
 
 		createAuthorizedRequest(
 			httpPrincipal,
@@ -55,7 +67,8 @@ public class StagingRemoteClient {
 	}
 
 	public static long createStagingRequest(
-		HttpPrincipal httpPrincipal, long targetGroupId, String checksum) {
+		HttpPrincipal httpPrincipal, long targetGroupId, String checksum)
+		throws AuthException, EncryptorException {
 
 		Form form = new Form();
 
@@ -72,7 +85,8 @@ public class StagingRemoteClient {
 
 	public static MissingReferencesRepr publishStagingRequest(
 		HttpPrincipal httpPrincipal, long stagingRequestId,
-		ExportImportConfiguration exportImportConfiguration) {
+		ExportImportConfiguration exportImportConfiguration)
+		throws AuthException, EncryptorException {
 
 		return createAuthorizedRequest(
 			httpPrincipal,
@@ -85,19 +99,88 @@ public class StagingRemoteClient {
 	}
 
 	private static Invocation.Builder createAuthorizedRequest(
-		HttpPrincipal httpPrincipal,
-		Function<WebTarget, Invocation.Builder> f) {
+			HttpPrincipal httpPrincipal,
+			Function<WebTarget, Invocation.Builder> f)
+		throws AuthException, EncryptorException {
 
 		WebTarget target = _clientBuilder.newClient().
 			target(httpPrincipal.getUrl());
 
 		Invocation.Builder builder = f.apply(target);
 
-		String authHeaderValue =
-			httpPrincipal.getLogin() + ":" + httpPrincipal.getPassword();
+		String encrypted = Encryptor.encrypt(
+			getSharedSecretKey(), httpPrincipal.getLogin());
 
-		return builder.header(
-			"Authorization", Base64.encode(authHeaderValue.getBytes()));
+		String authHeaderValue =
+			httpPrincipal.getLogin() + ":" + Base64.encode(
+				encrypted.getBytes());
+
+		return builder.header("Authorization", authHeaderValue);
+	}
+
+	protected static SecretKeySpec getSharedSecretKey() throws AuthException {
+		String sharedSecret = PropsValues.TUNNELING_SERVLET_SHARED_SECRET;
+		boolean sharedSecretHex =
+			PropsValues.TUNNELING_SERVLET_SHARED_SECRET_HEX;
+
+		if (Validator.isNull(sharedSecret)) {
+			String message =
+				"Please configure " + PropsKeys.TUNNELING_SERVLET_SHARED_SECRET;
+
+			AuthException authException = new AuthException(message);
+
+			authException.setType(AuthException.NO_SHARED_SECRET);
+
+			throw authException;
+		}
+
+		byte[] key = null;
+
+		if (sharedSecretHex) {
+			try {
+				key = Hex.decodeHex(sharedSecret.toCharArray());
+			}
+			catch (DecoderException de) {
+				AuthException authException = new AuthException();
+
+				authException.setType(AuthException.INVALID_SHARED_SECRET);
+
+				throw authException;
+			}
+		}
+		else {
+			key = sharedSecret.getBytes();
+		}
+
+		if (key.length < 8) {
+			String message =
+				PropsKeys.TUNNELING_SERVLET_SHARED_SECRET + " is too short";
+
+			AuthException authException = new AuthException(message);
+
+			authException.setType(AuthException.INVALID_SHARED_SECRET);
+
+			throw authException;
+		}
+
+		if (StringUtil.equalsIgnoreCase(
+			PropsValues.TUNNELING_SERVLET_ENCRYPTION_ALGORITHM, "AES") &&
+			(key.length != 16) && (key.length != 32)) {
+
+			String message =
+				PropsKeys.TUNNELING_SERVLET_SHARED_SECRET +
+				" must have 16 or 32 bytes when used with AES";
+
+			AuthException authException = new AuthException(message);
+
+			authException.setType(AuthException.INVALID_SHARED_SECRET);
+
+			throw authException;
+		}
+
+		return new SecretKeySpec(
+			key, PropsValues.TUNNELING_SERVLET_ENCRYPTION_ALGORITHM);
+
 	}
 
 	private static final ClientBuilder _clientBuilder;
