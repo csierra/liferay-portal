78cf9e6c3c498fec91cf8e83f848ef8067e46560
Tomáš Polešovský <tomas.polesovsky@liferay.com>
2016-10-12 12:40:33 +0200

    Illustration of double escaping in taglibs

--- portal-test/src/com/liferay/portal/kernel/security/test/XSSTest.java
+++ portal-test/src/com/liferay/portal/kernel/security/test/XSSTest.java
@@ -21,6 +21,9 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintWriter;
+
 /**
  * @author Carlos Sierra Andrés
  */
@@ -29,6 +32,89 @@ public class XSSTest {
 	@Before
 	public void setup() {
 		new HtmlUtil().setHtml(new HtmlImpl());
+	}
+
+	@Test
+	public void superSimpleTestToIllustrateDoubleEscaping() {
+		String original = "<a href=\"http://google.com\"> This is an XSS </a>";
+
+		XSS.SafeHtml safeHtml = XSS.htmlBody(XSS.attribute(original));
+
+		Assert.assertEquals(
+			safeHtml.toString(), HtmlUtil.escape(original));
+	}
+
+	@Test
+	public void testToIllustrateDoubleEscapingThatHappensInTaglibs() {
+		String articleName = " ' onclick=alert(1)";
+
+		XSS.SafeHtml escapedTooEarly_forExampleByAutoEscapeBeanHandler_beforePassingToTaglib
+			= XSS.htmlBody(articleName);
+
+		CharSequence labelForATag = escapedTooEarly_forExampleByAutoEscapeBeanHandler_beforePassingToTaglib;
+
+		/*
+		*
+		* Before the planned changes in taglib
+		*
+		 */
+
+		String outputSentToBrowser =
+			taglibCallBeforeOurChanges(labelForATag.toString());
+
+		// => make sure it's safe
+		Assert.assertTrue(!outputSentToBrowser.contains("'"));
+
+		// => but test double escaping -> escaped '&' char
+		Assert.assertTrue(!outputSentToBrowser.contains(HtmlUtil.escape("&")));
+		Assert.assertTrue(!outputSentToBrowser.contains(HtmlUtil.escapeAttribute("&")));
+
+		/*
+		*
+		* After the planned changes in taglib
+		*
+		 */
+		outputSentToBrowser =
+			taglibCallAfterOurChanges(labelForATag);
+
+		// => make sure it's safe
+		Assert.assertTrue(!outputSentToBrowser.contains("'"));
+
+		// => but test double escaping -> escaped '&' char
+		Assert.assertTrue(!outputSentToBrowser.contains(HtmlUtil.escape("&")));
+		Assert.assertTrue(!outputSentToBrowser.contains(HtmlUtil.escapeAttribute("&")));
+	}
+
+	protected String taglibCallBeforeOurChanges(String label) {
+		PrintWriter jspWriter = new PrintWriter(new ByteArrayOutputStream());
+
+		jspWriter.print("<a title='");
+		jspWriter.print(label);
+		jspWriter.print("'>");
+
+		// =>
+
+		String outputSentToBrowser = label;
+		return outputSentToBrowser;
+	}
+
+	protected String taglibCallAfterOurChanges(CharSequence label) {
+		PrintWriter jspWriter = new PrintWriter(new ByteArrayOutputStream());
+
+		jspWriter.print("<a title='");
+		// after the change we want to make sure the output is always correctly escaped
+		jspWriter.print(XSS.attribute(label));
+		jspWriter.print("'>");
+
+		// =>
+
+		CharSequence outputSendToBrowser = XSS.attribute(label);
+		return outputSendToBrowser.toString();
+	}
+
+	@Test
+	public void testx(){
+		System.out.println(HtmlUtil.escapeAttribute("javascript:alert(1)"));
 	}
 
 	@Test
