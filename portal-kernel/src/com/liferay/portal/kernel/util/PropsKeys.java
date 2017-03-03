6aa27439c1f2c35446eee575b3a7cc57090f0ac3
Carlos Sierra <carlos.sierra@liferay.com>
2017-03-03 18:11:52 +0100

    LPS-39160 Deprecate
    we should not allow mixed content

--- portal-kernel/src/com/liferay/portal/kernel/util/PropsKeys.java
+++ portal-kernel/src/com/liferay/portal/kernel/util/PropsKeys.java
@@ -403,6 +403,10 @@ public interface PropsKeys {
 
 	public static final String COMPANY_LOGIN_PREPOPULATE_DOMAIN = "company.login.prepopulate.domain";
 
+	/**
+	 * @deprecated As of 7.0.0, with no direct replacement
+	 */
+	@Deprecated
 	public static final String COMPANY_SECURITY_AUTH_REQUIRES_HTTPS = "company.security.auth.requires.https";
 
 	public static final String COMPANY_SECURITY_AUTH_TYPE = "company.security.auth.type";
