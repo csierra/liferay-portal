4607946456b2b03f92af0eeb4baa87cf6bdc8f1b
Alberto Chaparro <alberto.chaparro@liferay.com>
2018-02-06 19:50:19 +0100

    LPS-76926 We do not need to publish the version if the format is not valid. In case intermediate versions like X.X.X-step-X is working well because the bundle won't be available until completing all steps

--- modules/apps/foundation/portal/portal-upgrade/src/main/java/com/liferay/portal/upgrade/internal/release/ReleasePublisher.java
+++ modules/apps/foundation/portal/portal-upgrade/src/main/java/com/liferay/portal/upgrade/internal/release/ReleasePublisher.java
@@ -14,6 +14,8 @@
 
 package com.liferay.portal.upgrade.internal.release;
 
+import aQute.bnd.version.Version;
+
 import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.model.Release;
 import com.liferay.portal.kernel.module.framework.ModuleServiceLifecycle;
@@ -27,7 +29,6 @@ import java.util.Map;
 
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceRegistration;
-import org.osgi.framework.Version;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Deactivate;
@@ -53,9 +54,13 @@ public final class ReleasePublisher {
 
 		properties.put(
 			"release.bundle.symbolic.name", release.getBundleSymbolicName());
-		properties.put(
-			"release.schema.version", new Version(release.getSchemaVersion()));
 		properties.put("release.state", release.getState());
+
+		if (Version.isVersion(release.getSchemaVersion())) {
+			properties.put(
+				"release.schema.version",
+				new Version(release.getSchemaVersion()));
+		}
 
 		ServiceRegistration<Release> newServiceRegistration =
 			_bundleContext.registerService(Release.class, release, properties);
