078db47bda19cd2dbe38a3781225554e59b71c52
Stian Sigvartsen <stian.sigvartsen@liferay.com>
2018-02-16 12:36:09 +0000

    Remove dependency from ScopeMatcher to PrefixHander and ScopeMapper

--- modules/apps/oauth2-provider/oauth2-provider-scopes-spi/src/main/java/com/liferay/oauth2/provider/scopes/spi/ScopeMapper.java
+++ modules/apps/oauth2-provider/oauth2-provider-scopes-spi/src/main/java/com/liferay/oauth2/provider/scopes/spi/ScopeMapper.java
@@ -4,7 +4,21 @@ import java.util.Collections;
 import java.util.Set;
 
 public interface ScopeMapper {
-
+	
+	/**
+	 * Returns a new {@link ScopeMatcher} that takes into account the effect
+	 * of the given {@link ScopeMapper}. Some implementations might have
+	 * optimization opportunities.
+	 * @param scopeMatcher the scope matcher that should take into account this 
+	 * scope mapper.
+	 * @return the new {@link ScopeMatcher} that takes into account the given
+	 * {@link ScopeMapper}.
+	 */
+	public default ScopeMatcher withMapper(ScopeMatcher scopeMatcher) {
+		return localName ->
+			map(localName).stream().anyMatch(scopeMatcher::match);
+	}
+	
 	public Set<String> map(String scope);
 
 	public static ScopeMapper NULL = Collections::singleton;
