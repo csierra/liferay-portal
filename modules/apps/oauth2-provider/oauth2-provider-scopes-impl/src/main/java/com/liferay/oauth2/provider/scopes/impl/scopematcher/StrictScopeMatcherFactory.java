28b5a6655429455474aae4674b6c5a16be92d6e8
Tomáš Polešovský <tomas.polesovsky@liferay.com>
2018-02-20 11:52:47 +0100

    Extract StrictScopeMatcherFactory

--- modules/apps/oauth2-provider/oauth2-provider-scopes-impl/src/main/java/com/liferay/oauth2/provider/scopes/impl/scopematcher/StrictScopeMatcherFactory.java
+++ modules/apps/oauth2-provider/oauth2-provider-scopes-impl/src/main/java/com/liferay/oauth2/provider/scopes/impl/scopematcher/StrictScopeMatcherFactory.java
@@ -1,31 +1,34 @@
 /**
  * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
- * <p>
+ *
  * This library is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation; either version 2.1 of the License, or (at your option)
  * any later version.
- * <p>
+ *
  * This library is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
  */
 
-package com.liferay.oauth2.provider.scopes.spi;
+package com.liferay.oauth2.provider.scopes.impl.scopematcher;
 
 import com.liferay.oauth2.provider.scopes.scopematcher.ScopeMatcher;
+import com.liferay.oauth2.provider.scopes.spi.ScopeMatcherFactory;
+import org.osgi.service.component.annotations.Component;
 
 /**
- * Factory that creates {@link ScopeMatcher} for a given input.
- * This allow for components to switch matching strategies using configuration.
+ * @author Tomas Polesovsky
  */
-public interface ScopeMatcherFactory {
+@Component(
+	immediate = true,
+	property = "type=strict"
+)
+public class StrictScopeMatcherFactory implements ScopeMatcherFactory {
 
-	/**
-	 * Creates a {@link ScopeMatcher} for the given input.
-	 * @param input the input the matcher will match against.
-	 * @return the ScopeMatcher that will match against the input.
-	 */
-	ScopeMatcher create(String input);
+	@Override
+	public ScopeMatcher create(String input) {
+		return input::equals;
+	}
 }
