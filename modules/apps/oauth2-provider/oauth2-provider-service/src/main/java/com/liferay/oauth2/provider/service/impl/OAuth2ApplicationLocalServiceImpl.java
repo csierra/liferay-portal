4085903c82801e7ef716458b1f7124e5afeaa23b
Tomáš Polešovský <tomas.polesovsky@liferay.com>
2018-03-08 22:04:44 +0100

    OAuth2Application validations on the backend service layer

--- modules/apps/oauth2-provider/oauth2-provider-service/src/main/java/com/liferay/oauth2/provider/service/impl/OAuth2ApplicationLocalServiceImpl.java
+++ modules/apps/oauth2-provider/oauth2-provider-service/src/main/java/com/liferay/oauth2/provider/service/impl/OAuth2ApplicationLocalServiceImpl.java
@@ -32,11 +32,23 @@ import com.liferay.portal.kernel.portletfilerepository.PortletFileRepositoryUtil
 import com.liferay.portal.kernel.repository.model.FileEntry;
 import com.liferay.portal.kernel.repository.model.Folder;
 import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.util.Http;
+import com.liferay.portal.kernel.util.HttpUtil;
+import com.liferay.portal.kernel.util.PropsUtil;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.Validator;
 
 import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Date;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Objects;
+import java.util.Set;
 
 /**
  * The implementation of the o auth2 application local service.
@@ -71,11 +83,26 @@ public class OAuth2ApplicationLocalServiceImpl
 			List<String> scopesList, ServiceContext serviceContext)
 		throws PortalException {
 
-		if (oAuth2ApplicationPersistence.fetchByC_CI(
-				serviceContext.getCompanyId(), clientId) != null) {
-
-			throw new DuplicateOAuth2ClientIdException();
+		if (allowedGrantTypesList == null) {
+			allowedGrantTypesList = new ArrayList<>();
 		}
+
+		clientId = StringUtil.trim(clientId);
+		homePageURL = StringUtil.trim(homePageURL);
+		name = StringUtil.trim(name);
+		privacyPolicyURL = StringUtil.trim(privacyPolicyURL);
+
+		if (redirectURIsList == null) {
+			redirectURIsList = new ArrayList<>();
+		}
+
+		if (scopesList == null) {
+			scopesList = new ArrayList<>();
+		}
+
+		validate(
+			companyId, allowedGrantTypesList, clientConfidential, clientId,
+			clientSecret, homePageURL, name, privacyPolicyURL, redirectURIsList);
 
 		Date now = new Date();
 
@@ -172,6 +199,11 @@ public class OAuth2ApplicationLocalServiceImpl
 		OAuth2Application oAuth2Application =
 			oAuth2ApplicationPersistence.findByPrimaryKey(oAuth2ApplicationId);
 
+		validate(
+			oAuth2Application.getCompanyId(), oAuth2ApplicationId,
+			allowedGrantTypesList, clientConfidential, clientId, clientSecret,
+			homePageURL, name, privacyPolicyURL, redirectURIsList);
+
 		Date now = new Date();
 
 		oAuth2Application.setModifiedDate(now);
@@ -256,5 +288,143 @@ public class OAuth2ApplicationLocalServiceImpl
 
 		return oAuth2ApplicationPersistence.update(oAuth2Application);
 	}
+
+	protected void validate(
+			long companyId, List<GrantType> allowedGrantTypesList,
+			boolean clientConfidential, String clientId, String clientSecret,
+			String homePageURL, String name, String privacyPolicyURL,
+			List<String> redirectURIsList)
+		throws PortalException {
+
+		validate(
+			companyId, 0, allowedGrantTypesList, clientConfidential, clientId,
+			clientSecret, homePageURL, name, privacyPolicyURL,
+			redirectURIsList);
+	}
+
+	protected void validate(
+			long companyId, long oAuth2ApplicationId,
+			List<GrantType> allowedGrantTypesList, boolean clientConfidential,
+			String clientId, String clientSecret, String homePageURL,
+			String name, String privacyPolicyURL, List<String> redirectURIsList)
+		throws PortalException {
+
+		if (clientConfidential) {
+			for (GrantType grantType : allowedGrantTypesList) {
+				if (!grantType.isSupportsConfidentialClients()) {
+					throw new PortalException(grantType.name());
+				}
+			}
+		}
+		else {
+			for (GrantType grantType : allowedGrantTypesList) {
+				if (!grantType.isSupportsPublicClients()) {
+					throw new PortalException(grantType.name());
+				}
+			}
+		}
+
+		OAuth2Application duplicateApplication =
+			oAuth2ApplicationPersistence.fetchByC_CI(companyId, clientId);
+
+		if ((duplicateApplication != null) &&
+			(duplicateApplication.getOAuth2ApplicationId() !=
+			 oAuth2ApplicationId)) {
+
+			throw new DuplicateOAuth2ClientIdException();
+		}
+
+		if (Validator.isBlank(clientSecret) && clientConfidential) {
+			throw new PortalException("clientSecret");
+		}
+
+		if (!Validator.isBlank(homePageURL)) {
+			if (!StringUtil.startsWith(homePageURL, Http.HTTP_WITH_SLASH) &&
+				!StringUtil.startsWith(homePageURL, Http.HTTPS_WITH_SLASH)) {
+
+				throw new PortalException("homePageURL");
+			}
+
+			if (!Validator.isUri(homePageURL)) {
+				throw new PortalException("homePageURL");
+			}
+		}
+
+		if (Validator.isBlank(name)) {
+			throw new PortalException("name");
+		}
+
+		if (!Validator.isBlank(privacyPolicyURL)) {
+			if (!StringUtil.startsWith(
+					privacyPolicyURL, Http.HTTP_WITH_SLASH) &&
+				!StringUtil.startsWith(
+					privacyPolicyURL, Http.HTTPS_WITH_SLASH)) {
+
+				throw new PortalException("privacyPolicyURL");
+			}
+
+			if (!Validator.isUri(privacyPolicyURL)) {
+				throw new PortalException("privacyPolicyURL");
+			}
+		}
+
+		if (redirectURIsList.isEmpty()) {
+			for (GrantType grantType : allowedGrantTypesList) {
+				if (grantType.isRequiresRedirectURI()) {
+					throw new PortalException(grantType.name());
+				}
+			}
+		}
+
+		for (String redirectURI : redirectURIsList) {
+			try {
+				URI uri = new URI(redirectURI);
+
+				if (uri.getFragment() != null) {
+					throw new PortalException(redirectURI);
+				}
+
+				String scheme = uri.getScheme();
+
+				if (scheme == null) {
+					throw new PortalException(redirectURI);
+				}
+
+				scheme = scheme.toLowerCase();
+
+				if (!Objects.equals(scheme, Http.HTTP) &&
+					!Objects.equals(scheme, Http.HTTPS) &&
+					_IANA_REGISTERED_URI_SCHEMES.contains(scheme)) {
+
+					throw new PortalException(redirectURI);
+				}
+
+				String path = uri.getPath();
+				String normalizedPath = HttpUtil.normalizePath(path);
+
+				if (!Objects.equals(path, normalizedPath)) {
+					throw new PortalException(redirectURI);
+				}
+			}
+			catch (URISyntaxException e) {
+				throw new PortalException(redirectURI, e);
+			}
+		}
+	}
+
+	public void afterPropertiesSet() {
+		super.afterPropertiesSet();
+
+		String ianaRegisteredSchemes = PropsUtil.get(
+			"iana.registered.uri.schemes");
+
+		if (!Validator.isBlank(ianaRegisteredSchemes)) {
+			_IANA_REGISTERED_URI_SCHEMES =
+				new HashSet<>(
+					Arrays.asList(StringUtil.split(ianaRegisteredSchemes)));
+		}
+	}
+
+	private static Set<String> _IANA_REGISTERED_URI_SCHEMES = new HashSet<>(Arrays.asList(new String[] {"aaa", "aaas", "about", "acap", "acct", "acr", "adiumxtra", "afp", "afs", "aim", "appdata", "apt", "attachment", "aw", "barion", "beshare", "bitcoin", "blob", "bolo", "browserext", "callto", "cap", "chrome", "chrome-extension", "cid", "coap", "coap+tcp", "coap+ws", "coaps", "coaps+tcp", "coaps+ws", "com-eventbrite-attendee", "content", "conti", "crid", "cvs", "data", "dav", "diaspora", "dict", "dis", "dlna-playcontainer", "dlna-playsingle", "dns", "dntp", "dtn", "dvb", "ed2k", "example", "facetime", "fax", "feed", "feedready", "file", "filesystem", "finger", "fish", "ftp", "geo", "gg", "git", "gizmoproject", "go", "gopher", "graph", "gtalk", "h323", "ham", "hcp", "http", "https", "hxxp", "hxxps", "hydrazone", "iax", "icap", "icon", "im", "imap", "info", "iotdisco", "ipn", "ipp", "ipps", "irc", "irc6", "ircs", "iris", "iris.beep", "iris.lwz", "iris.xpc", "iris.xpcs", "isostore", "itms", "jabber", "jar", "jms", "keyparc", "lastfm", "ldap", "ldaps", "lvlt", "magnet", "mailserver", "mailto", "maps", "market", "message", "mid", "mms", "modem", "mongodb", "moz", "ms-access", "ms-browser-extension", "ms-drive-to", "ms-enrollment", "ms-excel", "ms-gamebarservices", "ms-gamingoverlay", "ms-getoffice", "ms-help", "ms-infopath", "ms-inputapp", "ms-lockscreencomponent-config", "ms-media-stream-id", "ms-mixedrealitycapture", "ms-officeapp", "ms-people", "ms-project", "ms-powerpoint", "ms-publisher", "ms-restoretabcompanion", "ms-search-repair", "ms-secondary-screen-controller", "ms-secondary-screen-setup", "ms-settings", "ms-settings-airplanemode", "ms-settings-bluetooth", "ms-settings-camera", "ms-settings-cellular", "ms-settings-cloudstorage", "ms-settings-connectabledevices", "ms-settings-displays-topology", "ms-settings-emailandaccounts", "ms-settings-language", "ms-settings-location", "ms-settings-lock", "ms-settings-nfctransactions", "ms-settings-notifications", "ms-settings-power", "ms-settings-privacy", "ms-settings-proximity", "ms-settings-screenrotation", "ms-settings-wifi", "ms-settings-workplace", "ms-spd", "ms-sttoverlay", "ms-transit-to", "ms-useractivityset", "ms-virtualtouchpad", "ms-visio", "ms-walk-to", "ms-whiteboard", "ms-whiteboard-cmd", "ms-word", "msnim", "msrp", "msrps", "mtqp", "mumble", "mupdate", "mvn", "news", "nfs", "ni", "nih", "nntp", "notes", "ocf", "oid", "onenote", "onenote-cmd", "opaquelocktoken", "pack", "palm", "paparazzi", "pkcs11", "platform", "pop", "pres", "prospero", "proxy", "pwid", "psyc", "qb", "query", "redis", "rediss", "reload", "res", "resource", "rmi", "rsync", "rtmfp", "rtmp", "rtsp", "rtsps", "rtspu", "secondlife", "service", "session", "sftp", "sgn", "shttp", "sieve", "sip", "sips", "skype", "smb", "sms", "smtp", "snews", "snmp", "soap.beep", "soap.beeps", "soldat", "spiffe", "spotify", "ssh", "steam", "stun", "stuns", "submit", "svn", "tag", "teamspeak", "tel", "teliaeid", "telnet", "tftp", "things", "thismessage", "tip", "tn3270", "tool", "turn", "turns", "tv", "udp", "unreal", "urn", "ut2004", "v-event", "vemmi", "ventrilo", "videotex", "vnc", "view-source", "wais", "webcal", "wpid", "ws", "wss", "wtai", "wyciwyg", "xcon", "xcon-userid", "xfire", "xmlrpc.beep", "xmlrpc.beeps", "xmpp", "xri", "ymsgr", "z39.50", "z39.50r", "z39.50s"}));
 
 }
