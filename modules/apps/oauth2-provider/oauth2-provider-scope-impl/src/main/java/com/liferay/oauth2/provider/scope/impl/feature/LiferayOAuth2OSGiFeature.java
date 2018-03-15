a6e35dcb147ebe055328619df81de11938cc0791
Stian Sigvartsen <stian.sigvartsen@liferay.com>
2018-03-15 13:46:54 +0000

    Fix regression (dbdb9d8) preventing osgi.jaxrs.name to be picked up from Application service properties

--- modules/apps/oauth2-provider/oauth2-provider-scope-impl/src/main/java/com/liferay/oauth2/provider/scope/impl/feature/LiferayOAuth2OSGiFeature.java
+++ modules/apps/oauth2-provider/oauth2-provider-scope-impl/src/main/java/com/liferay/oauth2/provider/scope/impl/feature/LiferayOAuth2OSGiFeature.java
@@ -25,10 +25,12 @@ import com.liferay.oauth2.provider.scope.spi.application.descriptor.ApplicationD
 import com.liferay.oauth2.provider.scope.spi.scope.descriptor.ScopeDescriptor;
 import com.liferay.oauth2.provider.scope.spi.scope.finder.ScopeFinder;
 import com.liferay.osgi.util.ServiceTrackerFactory;
+import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.ResourceBundleLoader;
 import com.liferay.portal.kernel.util.ResourceBundleUtil;
 import com.liferay.portal.kernel.util.StringBundler;
+import com.liferay.portal.kernel.util.Validator;
 
 import java.util.Arrays;
 import java.util.Collection;
@@ -205,8 +207,6 @@ public class LiferayOAuth2OSGiFeature implements Feature {
 
 				Class<? extends Application> clazz = application.getClass();
 
-				String applicationClassName = clazz.getName();
-
 				Bundle bundle = FrameworkUtil.getBundle(clazz);
 
 				if (bundle == null) {
@@ -250,7 +250,14 @@ public class LiferayOAuth2OSGiFeature implements Feature {
 						property, serviceReference.getProperty(property));
 				}
 
-				serviceProperties.put("osgi.jaxrs.name", applicationClassName);
+				String osgiJaxrsName = GetterUtil.getString(
+					serviceProperties.get("osgi.jaxrs.name"));
+
+				if (Validator.isNull(osgiJaxrsName)) {
+					osgiJaxrsName = clazz.getName();
+
+					serviceProperties.put("osgi.jaxrs.name", osgiJaxrsName);
+				}
 
 				if (oauth2ScopeCheckerType.equals("request.operation")) {
 					processRequestOperationStrategy(
@@ -264,7 +271,7 @@ public class LiferayOAuth2OSGiFeature implements Feature {
 						endpoint, applicationClassName, serviceProperties);
 				}
 
-				_registerDescriptors(endpoint, bundle, applicationClassName);
+				_registerDescriptors(endpoint, bundle, osgiJaxrsName);
 			}
 		}
 
