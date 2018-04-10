c82c57b1aaa1b49486df2e5b025dea5b6353165f
Tomáš Polešovský <tomas.polesovsky@liferay.com>
2018-04-10 17:02:23 +0200

    Fix deadlock

--- modules/apps/oauth2-provider/oauth2-provider-scope-impl/src/main/java/com/liferay/oauth2/provider/scope/internal/ScopeLocatorImpl.java
+++ modules/apps/oauth2-provider/oauth2-provider-scope-impl/src/main/java/com/liferay/oauth2/provider/scope/internal/ScopeLocatorImpl.java
@@ -230,8 +230,15 @@ public class ScopeLocatorImpl implements ScopeLocator {
 
 	@SuppressWarnings("unchecked")
 	private <T> T _getFromCacheOr(String key, Supplier<T> supplier) {
-		return (T)_invocationResultCache.computeIfAbsent(
-			key, __ -> supplier.get());
+		T value = (T)_invocationResultCache.get(key);
+
+		if (value == null) {
+			value = supplier.get();
+
+			_invocationResultCache.put(key, value);
+		}
+
+		return value;
 	}
 
 	private Collection<LiferayOAuth2Scope> _getLiferayOAuth2Scopes(
