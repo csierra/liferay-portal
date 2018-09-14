e86643eb17e471f2c50a20783a6d56eac7f8a86e
Carlos Sierra <carlos.sierra@liferay.com>
2018-09-14 15:10:40 +0200

    LPS-81573 Add support for file install .config

---
+++ modules/apps/portal-configuration/portal-configuration-extender/src/main/java/com/liferay/portal/configuration/extender/internal/ConfigConfigurationContentSupplierFactory.java
@@ -0,0 +1,48 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.configuration.extender.internal;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import java.util.Dictionary;
+import java.util.function.Supplier;
+
+import org.apache.felix.cm.file.ConfigurationHandler;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Carlos Sierra Andrés
+ */
+@Component(property = "type=config")
+public class ConfigConfigurationContentSupplierFactory
+	implements ConfigurationContentSupplierFactory {
+
+	@Override
+	public Supplier<Dictionary<String, Object>> create(
+		InputStream inputStream) {
+
+		return () -> {
+			try {
+				return ConfigurationHandler.read(inputStream);
+			}
+			catch (IOException ioe) {
+				throw new RuntimeException(ioe);
+			}
+		};
+	}
+
+}
