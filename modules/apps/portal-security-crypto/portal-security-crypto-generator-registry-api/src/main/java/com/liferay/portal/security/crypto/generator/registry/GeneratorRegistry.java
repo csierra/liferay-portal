d45a05731477ec7dc9d67215b7f7f3c37568fbd8
arthurchan35 <arthurchan35@gmail.com>
2019-08-28 15:55:16 +0800

    LPS-105024 API and Impl for Generator Registry

---
+++ modules/apps/portal-security-crypto/portal-security-crypto-generator-registry-api/src/main/java/com/liferay/portal/security/crypto/generator/registry/GeneratorRegistry.java
@@ -0,0 +1,34 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.security.crypto.generator.registry;
+
+import com.liferay.portal.security.crypto.generator.spi.HashGenerator;
+
+import org.json.JSONObject;
+
+/**
+ * @author Arthur Chan
+ */
+public interface GeneratorRegistry {
+
+	public HashGenerator getHashGenerator(
+			String generatorName, JSONObject generatorMeta)
+		throws Exception;
+
+	public HashGenerator getHashGenerator(
+			String generatorName, String generatorMeta)
+		throws Exception;
+
+}
