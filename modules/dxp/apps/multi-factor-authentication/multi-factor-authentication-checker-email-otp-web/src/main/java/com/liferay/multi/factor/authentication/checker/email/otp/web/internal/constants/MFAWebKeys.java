76a57b0f96596f480b583137a3e9287abb65387a
Carlos Sierra Andrés <carlos.sierra@liferay.com>
2020-01-07 17:32:25 +0100

    LPS-101720 Add Email One-Time password checker

---
+++ modules/dxp/apps/multi-factor-authentication/multi-factor-authentication-checker-email-otp-web/src/main/java/com/liferay/multi/factor/authentication/checker/email/otp/web/internal/constants/MFAWebKeys.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.multi.factor.authentication.checker.email.otp.web.internal.constants;
+
+/**
+ * @author Carlos Sierra Andrés
+ */
+public class MFAWebKeys {
+
+	public static final String MFA_EMAIL_OTP_CONFIGURATION =
+		"MFA_EMAIL_OTP_CONFIGURATION";
+
+	public static final String MFA_OTP = "MFA_OTP";
+
+	public static final String MFA_OTP_PHASE = "MFA_OTP_PHASE";
+
+	public static final String MFA_OTP_SET_AT = "MFA_OTP_SET_AT";
+
+	public static final String MFA_SEND_TO_EMAIL_ADDRESS =
+		"MFA_SEND_TO_EMAIL_ADDRESS";
+
+	public static final String MFA_USER_ID = "MFA_USER_ID";
+
+	public static final String MFA_VALIDATED_AT = "MFA_VALIDATED_AT";
+
+	public static final String MFA_VALIDATED_USER_ID = "MFA_VALIDATED_USER_ID";
+
+}
