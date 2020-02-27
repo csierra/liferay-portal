c81f9a83bc522832160274cb30febc833f31f082
arthurchan35 <arthurchan35@gmail.com>
2020-02-27 16:18:52 +0800

    Demo provider - Message Digest

---
+++ modules/apps/portal-crypto/portal-crypto-hash-generator-provider-message-digest/src/main/java/com/liferay/portal/crypto/hash/generator/provider/message/digest/MessageDigestHashGenerator.java
@@ -0,0 +1,55 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.crypto.hash.generator.provider.message.digest;
+
+import com.liferay.portal.crypto.hash.generator.spi.HashGenerator;
+import com.liferay.portal.crypto.hash.generator.spi.VariableSizeSaltHashGenerator;
+
+import java.security.MessageDigest;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Arthur Chan
+ */
+@Component(immediate = true, service = HashGenerator.class)
+public class MessageDigestHashGenerator extends VariableSizeSaltHashGenerator {
+
+	public MessageDigestHashGenerator() {
+		_chosenGenerator = "SHA-512";
+	}
+
+	public MessageDigestHashGenerator(String generatorName) {
+		_chosenGenerator = generatorName;
+	}
+
+	@Override
+	public byte[] hash(byte[] input) throws Exception {
+		MessageDigest messageDigest = MessageDigest.getInstance(
+			_chosenGenerator);
+
+		byte[] combined = new byte[input.length + pepper.length + salt.length];
+
+		System.arraycopy(input, 0, combined, 0, input.length);
+		System.arraycopy(pepper, 0, combined, input.length, pepper.length);
+		System.arraycopy(
+			salt, 0, combined, input.length + pepper.length, salt.length);
+
+		return messageDigest.digest(combined);
+	}
+
+	private final String _chosenGenerator;
+
+}
