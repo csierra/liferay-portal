e5115ac8ff071cf363aaf0773a324ea1fc0c0704
Carlos Sierra Andrés <carlos.sierra@liferay.com>
2020-02-28 14:50:21 +0800

    LPS-105024 Use HashRequest and HashResponse to wrap raw material

---
+++ modules/apps/portal-crypto/portal-crypto-hash-api/src/main/java/com/liferay/portal/crypto/hash/request/command/salt/GenerateVariableSizeSaltCommand.java
@@ -0,0 +1,37 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.crypto.hash.request.command.salt;
+
+/**
+ * @author Carlos Sierra Andrés
+ */
+public class GenerateVariableSizeSaltCommand implements SaltCommand {
+
+	public GenerateVariableSizeSaltCommand(int size) {
+		_size = size;
+	}
+
+	@Override
+	public <T> T accept(SaltCommandVisitor<T> visitor) {
+		return visitor.visit(this);
+	}
+
+	public int getSaltSize() {
+		return _size;
+	}
+
+	private final int _size;
+
+}
