f627ac0263c5b87f864d6ae829d32b6a74bbeaa8
arthurchan35 <arthurchan35@gmail.com>
2020-03-05 14:18:17 +0800

    LPS-105024 Crypto Hash Impl:
    Implementation for Crypto Hash API
    
    Co-authored-by: Carlos Sierra Andrés <carlos.sierra@liferay.com>

---
+++ modules/apps/portal-crypto-hash/portal-crypto-hash-impl/src/main/java/com/liferay/portal/crypto/hash/internal/verification/context/HashVerificationContextImpl.java
@@ -0,0 +1,158 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.crypto.hash.internal.verification.context;
+
+import com.liferay.portal.crypto.hash.flavor.HashFlavor;
+import com.liferay.portal.crypto.hash.internal.flavor.HashFlavorImpl;
+import com.liferay.portal.crypto.hash.verification.context.HashVerificationContext;
+
+import java.io.ByteArrayInputStream;
+import java.io.DataInputStream;
+import java.io.IOException;
+
+import java.util.Optional;
+
+import org.json.JSONObject;
+
+/**
+ * @author Arthur Chan
+ */
+public class HashVerificationContextImpl implements HashVerificationContext {
+
+	public HashVerificationContextImpl(
+		String hashGeneratorName, JSONObject hashGeneratorMeta,
+		HashFlavor hashFlavor) {
+
+		_hashGeneratorName = hashGeneratorName;
+		_hashGeneratorMeta = Optional.ofNullable(hashGeneratorMeta);
+
+		_hashFlavor = hashFlavor;
+	}
+
+	@Override
+	public HashFlavor getHashFlavor() {
+		return _hashFlavor;
+	}
+
+	@Override
+	public Optional<JSONObject> getHashGeneratorMeta() {
+		return _hashGeneratorMeta;
+	}
+
+	@Override
+	public String getHashGeneratorName() {
+		return _hashGeneratorName;
+	}
+
+	public static class BuilderImpl implements HashVerificationContext.Builder {
+
+		public BuilderImpl(
+			String hashGeneratorName, JSONObject hashGeneratorMeta) {
+
+			this(hashGeneratorName, hashGeneratorMeta, null, null);
+		}
+
+		public BuilderImpl(
+			String hashGeneratorName, JSONObject hashGeneratorMeta,
+			String pepperId, byte[] salt) {
+
+			_hashGeneratorName = hashGeneratorName;
+			_hashGeneratorMeta = hashGeneratorMeta;
+			_pepperId = pepperId;
+			_salt = salt;
+		}
+
+		@Override
+		public HashVerificationContext build() {
+			HashFlavor hashFlavor = new HashFlavorImpl(_pepperId, _salt);
+
+			return new HashVerificationContextImpl(
+				_hashGeneratorName, _hashGeneratorMeta, hashFlavor);
+		}
+
+		@Override
+		public HashVerificationContext build(byte[] serializedHashFlavor)
+			throws IOException {
+
+			DataInputStream dataInputStream = new DataInputStream(
+				new ByteArrayInputStream(serializedHashFlavor));
+
+			byte b = dataInputStream.readByte();
+
+			if (b != 1) {
+				throw new IllegalArgumentException(
+					"Version " + b + " is not supported");
+			}
+
+			String pepperId = dataInputStream.readUTF();
+
+			int unsignedByte = dataInputStream.readUnsignedShort();
+
+			byte[] salt = new byte[unsignedByte];
+
+			dataInputStream.readFully(salt);
+
+			return new HashVerificationContextImpl(
+				_hashGeneratorName, _hashGeneratorMeta,
+				new HashFlavorImpl(pepperId, salt));
+		}
+
+		@Override
+		public HashVerificationContext build(HashFlavor hashFlavor) {
+			Optional<String> optionalPepperId = hashFlavor.getPepperId();
+
+			Optional<byte[]> optionalsalt = hashFlavor.getSalt();
+
+			return new HashVerificationContextImpl(
+				_hashGeneratorName, _hashGeneratorMeta,
+				new HashFlavorImpl(
+					optionalPepperId.orElse(""),
+					optionalsalt.orElse(new byte[0])));
+		}
+
+		@Override
+		public SaltedBuilder pepper(String pepperId) {
+			if (pepperId == null) {
+				throw new IllegalArgumentException(
+					"use other overloaded method if no pepperId is provided");
+			}
+
+			return new BuilderImpl(
+				_hashGeneratorName, _hashGeneratorMeta, pepperId, _salt);
+		}
+
+		@Override
+		public Builder salt(byte[] salt) {
+			if (salt == null) {
+				throw new IllegalArgumentException(
+					"use other overloaded method if no salt is provided");
+			}
+
+			return new BuilderImpl(
+				_hashGeneratorName, _hashGeneratorMeta, _pepperId, salt);
+		}
+
+		private final JSONObject _hashGeneratorMeta;
+		private final String _hashGeneratorName;
+		private final String _pepperId;
+		private final byte[] _salt;
+
+	}
+
+	private final HashFlavor _hashFlavor;
+	private final Optional<JSONObject> _hashGeneratorMeta;
+	private final String _hashGeneratorName;
+
+}
