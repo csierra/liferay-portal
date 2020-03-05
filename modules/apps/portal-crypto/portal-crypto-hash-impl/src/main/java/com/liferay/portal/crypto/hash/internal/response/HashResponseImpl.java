ce7a6ce5c8981789afcc7157692c116cfeeef653
arthurchan35 <arthurchan35@gmail.com>
2020-03-05 14:18:17 +0800

    LPS-105024 Add impl for HashResponse

---
+++ modules/apps/portal-crypto/portal-crypto-hash-impl/src/main/java/com/liferay/portal/crypto/hash/internal/response/HashResponseImpl.java
@@ -0,0 +1,48 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.crypto.hash.internal.response;
+
+import com.liferay.portal.crypto.hash.response.HashResponse;
+
+import java.util.Optional;
+
+/**
+ * @author Arthur Chan
+ */
+public class HashResponseImpl implements HashResponse {
+
+	public HashResponseImpl(byte[] hash, byte[] pepper, byte[] salt) {
+		_hash = hash;
+		_pepper = Optional.ofNullable(pepper);
+		_salt = Optional.ofNullable(salt);
+	}
+
+	public byte[] getHash() {
+		return _hash;
+	}
+
+	public Optional<byte[]> getPepper() {
+		return _pepper;
+	}
+
+	public Optional<byte[]> getSalt() {
+		return _salt;
+	}
+
+	private final byte[] _hash;
+	private final Optional<byte[]> _pepper;
+	private final Optional<byte[]> _salt;
+
+}
