be3decdc35ba127ad5048c017146cb262fe16da3
arthurchan35 <arthurchan35@gmail.com>
2020-03-31 12:38:15 +0800

    LPS-111202 The comparison needs able to prevent timing attack, see:
    https://en.wikipedia.org/wiki/Timing_attack

--- modules/apps/portal-crypto-hash/portal-crypto-hash-impl/src/main/java/com/liferay/portal/crypto/hash/internal/processor/HashProcessorImpl.java
+++ modules/apps/portal-crypto-hash/portal-crypto-hash-impl/src/main/java/com/liferay/portal/crypto/hash/internal/processor/HashProcessorImpl.java
@@ -104,7 +104,23 @@ public class HashProcessorImpl implements HashProcessor {
 			input = hashProvider.hash(input);
 		}
 
-		return Arrays.equals(input, hash);
+		return _compare(input, hash);
+	}
+
+	// A comparison algorithm that prevents timing attack
+
+	private boolean _compare(byte[] bytes1, byte[] bytes2) {
+		int diff = bytes1.length ^ bytes2.length;
+
+		for (int i = 0; (i < bytes1.length) && (i < bytes2.length); ++i) {
+			diff |= bytes1[i] ^ bytes2[i];
+		}
+
+		if (diff == 0) {
+			return true;
+		}
+
+		return false;
 	}
 
 	@Activate
