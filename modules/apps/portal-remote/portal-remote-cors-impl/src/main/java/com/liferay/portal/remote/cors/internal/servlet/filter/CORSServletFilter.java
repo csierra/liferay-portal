f9b38307d6d69e16e4370a8ab6821581a2295596
Carlos Sierra Andrés <carlos.sierra@liferay.com>
2020-06-29 15:13:59 +0800

    LPS-111617 Access static method statically

--- modules/apps/portal-remote/portal-remote-cors-impl/src/main/java/com/liferay/portal/remote/cors/internal/servlet/filter/CORSServletFilter.java
+++ modules/apps/portal-remote/portal-remote-cors-impl/src/main/java/com/liferay/portal/remote/cors/internal/servlet/filter/CORSServletFilter.java
@@ -42,7 +42,7 @@ public class CORSServletFilter extends BaseFilter {
 		HttpServletRequest httpServletRequest,
 		HttpServletResponse httpServletResponse) {
 
-		if (corsSupport.isCORSRequest(httpServletRequest::getHeader)) {
+		if (CORSSupport.isCORSRequest(httpServletRequest::getHeader)) {
 			return true;
 		}
 
