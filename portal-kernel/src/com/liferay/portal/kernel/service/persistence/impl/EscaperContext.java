5a782d39d1dfb8d6aa0b40ab7da4444202111507
Carlos Sierra Andrés <carlos.sierra@liferay.com>
2020-12-11 17:45:01 +0100

    WIP

---
+++ portal-kernel/src/com/liferay/portal/kernel/service/persistence/impl/EscaperContext.java
@@ -0,0 +1,31 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ * <p>
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ */
+
+package com.liferay.portal.kernel.service.persistence.impl;
+
+import com.liferay.petra.lang.CentralizedThreadLocal;
+
+/**
+ * @author Carlos Sierra Andrés
+ */
+public final class EscaperContext {
+
+	public static void setEscaper(Escaper escaper) {
+		_escaperThreadLocal.set(escaper);
+	}
+
+	public static Escaper getEscaper() {
+		return _escaperThreadLocal.get();
+	}
+
+	private static ThreadLocal<Escaper> _escaperThreadLocal =
+		new CentralizedThreadLocal<>("_escaperThreadLocal");
+}
---
+++ portal-kernel/src/com/liferay/portal/kernel/service/persistence/impl/UserInputString.java
@@ -0,0 +1,72 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ * <p>
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ */
+
+package com.liferay.portal.kernel.service.persistence.impl;
+
+import java.util.function.Function;
+import java.util.stream.IntStream;
+
+/**
+ * @author Carlos Sierra Andrés
+ */
+public final class UserInputString implements CharSequence {
+
+	private final String _string;
+
+	String getString() {
+		return _string;
+	}
+
+	public UserInputString(String string) {
+		_string = string;
+	}
+
+	@Override
+	public String toString() {
+		Escaper escaper = EscaperContext.getEscaper();
+
+		if (escaper == null) {
+			throw new IllegalStateException("No escape context available");
+		}
+
+		return escaper.escape(_string);
+	}
+
+	@Override
+	public int length() {
+		return toString().length();
+	}
+
+	@Override
+	public char charAt(int index) {
+		return toString().charAt(index);
+	}
+
+	@Override
+	public CharSequence subSequence(int start, int end) {
+		return toString().subSequence(start, end);
+	}
+
+	@Override
+	public IntStream chars() {
+		return toString().chars();
+	}
+
+	@Override
+	public IntStream codePoints() {
+		return toString().codePoints();
+	}
+
+	public UserInputString map(Function<String, String> function) {
+		return new UserInputString(function.apply(_string));
+	}
+
+}
